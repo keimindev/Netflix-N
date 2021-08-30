c588de35f6a8644c1a2d320b1fca67c0ed6a84e2
min <keiramingj@gmail.com>
2021-08-31 01:36:40 +0900

    1.Conneted MongooDB / Build data

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+//MongoDB connection
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users'); 
+const moviesRoute = require('./routes/movies'); 
+const listsRoute = require('./routes/lists'); 
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL , {
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+})
+.then(() => console.log('DB Connection Successful!!'))
+.catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/movies", moviesRoute)
+app.use("/api/lists", listsRoute)
+
+app.listen(8800, () => {
+    console.log("backend server is running")
+})
